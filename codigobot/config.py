c9038d14db4fb705b6d035e1cae6cdf1cbb18600
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2025-01-24 11:29:20 -0300

    Update config.py

--- codigobot/config.py
+++ codigobot/config.py
@@ -1,9 +1,3 @@
-import openai
-
 with open('C:/Users/User Name/Projetos/codigobot/token.txt', 'r') as file: #MUDE O CAMINHO "C:/Users/User Name/Projetos/codigobot/token.txt" PARA O LOCAL DO BOT
     tokens = file.readlines()
     token = tokens[0].strip()  # TOKEN DO BOT DISCORD do bot Discord
-    openai_key = tokens[1].strip()  # CHAVE DA API DO OPENAI
-
-# DEFINIR A CHAVE DA API DO OPENAI
-openai.api_key = openai_key
